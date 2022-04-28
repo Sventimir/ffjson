efb69c6a4173e84964c81afb078caeac765dbdff
Marcin Pastudzki <marcin.pastudzki@gmail.com>
2022-04-28 20:32:51 +0200

    Add a possibility to name input channels.

--- app/Main.hs
+++ app/Main.hs
@@ -26,12 +26,15 @@ import Text.Megaparsec (ParseErrorBundle)
 
 data FFJsonError = UnexpectedPositional String
                  | InputError InputError
+                 | NoInput
+                 | AlreadyNamed String
                  | ParseError (ParseErrorBundle Text ParseError)
                  deriving Show
 
 instance Exception FFJsonError
 
 data Config = Config {
+    currentInput :: Maybe (Maybe String, Input),
     inputs :: Inputs,
     indentation :: Int
   }
@@ -39,20 +42,38 @@ data Config = Config {
 setIndentation :: Int -> Config -> Config
 setIndentation i cfg = cfg { indentation = i }
 
-selectInput :: String -> Config -> ExceptTraceT IO Config
-selectInput flag cfg = do
+addNewInput :: String -> Config -> ExceptTraceT IO Config
+addNewInput flag cfg = do
   inp <- parseInput flag
-  return $ cfg { inputs = addInput Nothing inp $ inputs cfg }
+  return $ (finalizeInput cfg) { currentInput = Just (Nothing, inp) }
+
+setInputName :: String -> Config -> ExceptTraceT IO Config
+setInputName name cfg = case currentInput cfg of
+  Nothing -> throwM NoInput
+  Just (Nothing, inp) -> return $ cfg { currentInput = Just (Just name, inp) }
+  Just (Just n, _) -> throwM $ AlreadyNamed n
+
+finalizeInput :: Config -> Config
+finalizeInput cfg = case currentInput cfg of
+  Nothing ->  cfg
+  Just (name, input) -> cfg {
+      currentInput = Nothing,
+      inputs = addInput name input $ inputs cfg
+    }
 
 instance CliArgs (ExceptTraceT IO) Config where
-  defaults = Config emptyInputs 2
-  finalize = return
+  defaults = Config Nothing emptyInputs 2
+  finalize = return . finalizeInput
   positional _ = throwM . UnexpectedPositional
   hyphens _ len = throwM . UnexpectedPositional $ replicate len '-'
   flags = [
-            FlagSpec (OrBoth 'i' "input") (ArgS ArgZ) (\f cfg -> selectInput f cfg),
-            FlagSpec (OrBoth 'r' "raw") ArgZ (return . setIndentation 0),
-            FlagSpec (OrRight "indent") (ArgS ArgZ) (\i -> return . setIndentation (read i))
+            FlagSpec (OrBoth 'i' "input") (ArgS ArgZ) (\f cfg -> addNewInput f cfg),
+            FlagSpec (OrBoth 'n' "name") (ArgS ArgZ) setInputName,
+            FlagSpec (OrBoth 'r' "raw") ArgZ (return . setIndentation 0 . finalizeInput),
+            FlagSpec
+              (OrRight "indent")
+              (ArgS ArgZ)
+              (\i -> return . setIndentation (read i) . finalizeInput)
           ]
 
 parseJson :: (MonadIO m, JSON json) => Text -> ExceptTraceT m json
